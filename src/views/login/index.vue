ab503971638c95c7e6e7e94e167d0e1acee49e03
沙砾 <895523193@qq.com>
2018-12-28 11:21:55 +0800

    测试

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="login-container">
-
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
